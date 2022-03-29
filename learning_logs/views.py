9c8fc6ee7e89571e7328de86f3cef6c2eee73747
ehmatthes <eric@example.com>
2022-03-29 19:04:25 +0600

    404 add in Views.py

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 from django.contrib.auth.decorators import login_required
@@ -21,6 +21,7 @@ def topics(request):
 @login_required
 def topic(request, topic_id):
     """Выводит одну тему и все ее записи."""
+    topic = get_object_or_404(Topic, id=topic_id)
     topic = Topic.objects.get(id=topic_id)
     if topic.owner != request.user:
         raise Http404
